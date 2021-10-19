10d2ee95bfb010d995828c563f753f083b954ffe
Ccondir <87036822+Ccondir@users.noreply.github.com>
2021-10-19 16:38:53 +0200

    Add files via upload

---
+++ gejm.py
@@ -0,0 +1,27 @@
+import time
+
+a = str(input("u gay?: "))
+if a == ("yes") :
+    print("then don't be")
+elif a == ("no") :
+    print("congrats you are normal")
+else:
+    print("noly yes or no")
+b = str(input("wanna turn this off and your IPadress to be taken? "))
+if b == ("yes"):
+    print("ok thx")
+elif b == ("no"):
+    print("well too bad")
+else:
+    print("lol") 
+time.sleep(5)
+print("megahomo")
+time.sleep(600)
+print("proč tu čekáš?")
+time.sleep(6000)
+print("stop")
+time.sleep(10)
+print("zavři tuhle hru")
+time.sleep(30)
+print("prostě přestaň")
+time.sleep(60)
